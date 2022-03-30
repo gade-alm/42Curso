1c731e197f5ac62cb01ac9edc4d151e3398ef8fa
Gabriel De almeida torres <gade-alm@c1r10s1.42lisboa.com>
2022-03-30 16:43:42 +0100

    Update GNL

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,99 +5,118 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gade-alm <gade-alm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/22 12:12:32 by gade-alm          #+#    #+#             */
-/*   Updated: 2022/03/29 12:20:11 by gade-alm         ###   ########.fr       */
+/*   Created: 2022/03/30 11:03:28 by gade-alm          #+#    #+#             */
+/*   Updated: 2022/03/30 16:42:11 by gade-alm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "get_next_line.h"
+#include <stdio.h>
 #include <fcntl.h>
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\n')
-			return (i + 1);
-		i++;
-	}
-	return (i);
-}
-
-char	*get_line(char *line, char *buff)
+char	*get_line(char *str)
 {
 	int		i;
-	int		j;
-	char	*temp;
+	char	*s;
 
 	i = 0;
-	temp = line;
-	line = (char *)malloc(sizeof(char) * ft_strlen(buff) + ft_strlen(line));
-	if (!line)
+	while (str[i] && str[i] != '\n')
+		i++;
+	s = (char *)malloc(sizeof(*s) * (i + 1));
+	if (!s)
 		return (NULL);
-	while (temp && temp[i])
+	i = 0;
+	while (str[i] && str[i] != '\n')
 	{
-		line[i] = temp[i];
+		s[i] = str[i];
 		i++;
 	}
-	j = 0;
-	while (buff[j])
-	{
-		if (buff[j++] == '\n')
-			break ;
-		line[i++] = buff [j];
-	}
-	if (temp)
-		free (temp);
-	return (line);
+	if (str[i] == '\n')
+		s[i] = str[i];
+	s[i + 1] = '\0';
+	return (s);
 }
 
-//Find the next line and save it on another string
-// char	*save_next(char *save)
-// {
-// 	int			i;
-// 	int			j;
-// 	char		*str;
+char	*save_next(char *str)
+{
+	int			i;
+	int			j;
+	char		*save;
 
-// 	i = 0;
-// 	j = 0;
-// 	if (!save)
-// 		return (NULL);
-// 	while (*save && save[i] != '\n')
-// 		i++;
-// 	i += 1;
-// 	str = malloc(sizeof(char *) * (i + 1));
-// 	if (!str)
-// 		return (NULL);
-// 	while (save[i] && str[j] != '\n')
-// 	{
-// 		str[j] = save[i];
-// 		i++;
-// 		j++;
-// 	}
-// 	str[j] = '\0';
-// 	return (str);
-// }
+	i = 0;
+	j = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!str[i])
+	{
+		free(str);
+		return (NULL);
+	}
+	save = malloc(sizeof(char *) * (ft_strlen(str) - i + 1));
+	if (!save)
+		return (NULL);
+	i++;
+	while (str[i])
+		save[j++] = str[i++];
+	save[j] = '\0';
+	free (str);
+	return (save);
+}
+
+char	*read_line(int fd, char *str)
+{
+	char			*buff;
+	int				re;
+
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	re = 1;
+	while (!ft_strchr(str, '\n') && re != 0)
+	{
+		re = read(fd, buff, BUFFER_SIZE);
+		if (re == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+			buff[re] = '\0';
+			str = ft_strjoin(str, buff);
+	}
+	free (buff);
+	return (str);
+}
 
 char	*get_next_line(int fd)
 {
-	char		*line;
-	static char	buff[BUFFER_SIZE];
+	char		*str;
+	static char	*buff;
 
-	read = ("text.txt", buff, BUFFER_SIZE);
-	return (line);
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (0);
+	buff = read_line(fd, buff);
+	if (!buff)
+		return (NULL);
+	str = get_line(buff);
+	buff = save_next(buff);
+	return (str);
 }
+
 int	main(void)
 {
-	char	*s;
+	char	*line;
+	int		i;
+	int		fd1;
 
-	s = "teste\nabc";
-	printf("save_next:\n");
-	printf("%s\n", save_next(s));
-	printf("get_line:\n");
-	printf("%s", get_line(s));
+	fd1 = open("text", O_RDONLY);
+	i = 1;
+	while (i < 7)
+	{
+		line = get_next_line(fd1);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	return (0);
 }
