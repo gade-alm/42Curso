8b348c0d6feccd9e0168ba9d5deff04ef983eb2d
Gabriel De almeida torres <gade-alm@c1r10s3.42lisboa.com>
2022-04-05 12:25:44 +0100

    GNL

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -1,51 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gade-alm <gade-alm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/31 14:45:25 by gade-alm          #+#    #+#             */
-/*   Updated: 2022/04/05 11:49:20 by gade-alm         ###   ########.fr       */
+/*   Created: 2022/04/05 11:50:40 by gade-alm          #+#    #+#             */
+/*   Updated: 2022/04/05 12:10:11 by gade-alm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdio.h>
 
 char	*get_next_line(int fd)
 {
-	static char		buffer[BUFFER_SIZE + 1];
+	static char		buffer[FOPEN_MAX][BUFFER_SIZE];
 	int				re;
 	char			*line;
 
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE < 1 || fd >= FOPEN_MAX)
 		return (NULL);
 	line = NULL;
+	re = 1;
 	while (1)
 	{		
-		re = 1;
-		if (!buffer[0])
-			re = read(fd, buffer, BUFFER_SIZE);
+		if (!buffer[fd][0])
+			re = read(fd, buffer[fd], BUFFER_SIZE);
 		if (re > 0)
-			line = ft_get_line(buffer, line);
-		if (re < 1 || is_newline(buffer))
+			line = ft_get_line(buffer[fd], line);
+		if (re < 1 || is_newline(buffer[fd]))
 			break ;
 	}
 	return (line);
 }
-
-// int	main(void)
-// {
-// 	int		fd;
-// 	char	*str;
-
-// 	fd = open("text", O_RDONLY);
-// 	while(1)
-// 	{
-// 	str = get_next_line(fd);
-// 		if (!str)
-// 			break ;
-// 		printf("%s", str);
-// 	}
-// }
