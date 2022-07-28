563280f5d34efb4710dc4931190576fb7b869f30
Gabriel De almeida torres <gade-alm@c2r10s3.42lisboa.com>
2022-07-28 19:11:11 +0100

    Save

--- 2nd_circle/so_long/includes/so_long.h
+++ 2nd_circle/so_long/includes/so_long.h
@@ -1,28 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gade-alm <gade-alm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/28 16:54:52 by gade-alm          #+#    #+#             */
-/*   Updated: 2022/07/28 19:10:14 by gade-alm         ###   ########.fr       */
+/*   Created: 2022/07/28 18:34:12 by gade-alm          #+#    #+#             */
+/*   Updated: 2022/07/28 18:45:01 by gade-alm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx_linux/mlx.h"
+#ifndef 	SO_LONG_H
+# define	SO_LONG_H
 
+typedef struct s_data{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
 
-int main(void)
-{
-	void	*mlx;
-	char	*relative_path = "2nd_circle/so_long/imgs/grass.xpm";
-	int		img_width;
-	int		img_height;
-	
-	mlx = mlx_init();
-	mlx_new_window(mlx, 960, 480, "teste");
-	mlx_new_image(mlx, 960, 480);
-	mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
-	mlx_loop(mlx);
-}
+#endif
